45281a59b65da8b47504c63d48f34c4278d5fccf
Peter Daum <pedaum@web.de>
2012-04-04 00:28:19 +0200

    Added Apache v2.0 licensing

--- src/main/java/de/betaphitheta/bulkinstaller/BulkInstaller.java
+++ src/main/java/de/betaphitheta/bulkinstaller/BulkInstaller.java
@@ -1,6 +1,17 @@
 /*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
+ *Copyright 2012 Peter Daum
+ *
+ *Licensed under the Apache License, Version 2.0 (the "License");
+ *you may not use this file except in compliance with the License.
+ *You may obtain a copy of the License at
+ *
+ *http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *Unless required by applicable law or agreed to in writing, software
+ *distributed under the License is distributed on an "AS IS" BASIS,
+ *WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *See the License for the specific language governing permissions and
+ *limitations under the License.
  */
 package de.betaphitheta.bulkinstaller;
 
@@ -12,6 +23,7 @@ import org.osgi.framework.BundleException;
 import org.apache.felix.service.command.Descriptor;
 
 /**
+ * 
  * This class represents the implementation of a bulk install command for the gogo shell of Apache Felix.
  * 
  * It provides to methods. One for installing bundles from a directory, where the jar files matches a given pattern.
